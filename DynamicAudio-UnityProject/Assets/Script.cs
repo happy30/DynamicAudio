de96feb166babe3f6de0527fa74550900ed8df54
Faf-Desktop <jordivdh@hotmail.com>
2017-08-20 17:43:43 +0200

    Initial commit

---
+++ DynamicAudio-UnityProject/Assets/Script.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Script : MonoBehaviour
+{
+    public Text[] intensityText;
+    public Image[] intensityBackground;
+
+    public Text calmSourceText;
+    public Text quietSourceText;
+    public Text dynamicSourceText;
+
+    public float fillSpeed;
+
+    public void OnButtonClick(int button)
+    {
+        if(!Camera.main.GetComponent<SoundManager>().quietSource.isPlaying)
+        {
+            Camera.main.GetComponent<SoundManager>().quietSource.Play();
+            Camera.main.GetComponent<SoundManager>().mediumSource.Play();
+            Camera.main.GetComponent<SoundManager>().dynamicSource.Play();
+        }
+
+        Camera.main.GetComponent<SoundManager>().SetLevel((SoundManager.AudioLevel)button);
+
+
+        foreach (Image intbg in intensityBackground)
+        {
+            if(intbg.fillAmount > 0.2f)
+            {
+                StartCoroutine(FillBackground(intbg, false));
+            }
+        }
+
+        foreach (Text intText in intensityText)
+        {
+            intText.color = new Color(0.5f, 0.5f, 0.5f);
+        }
+        intensityText[button].color = new Color(0.1f, 0.1f, 0.1f);
+
+        StartCoroutine(FillBackground(intensityBackground[button], true));
+
+        if(button == 0)
+        {
+            quietSourceText.color = new Color(0.9f, 0.9f, 0.9f);
+            calmSourceText.color = new Color(0.25f, 0.25f, 0.25f);
+            dynamicSourceText.color = new Color(0.25f, 0.25f, 0.25f);
+        }
+        else if(button == 1)
+        {
+            quietSourceText.color = new Color(0.25f, 0.25f, 0.25f);
+            calmSourceText.color = new Color(0.9f, 0.9f, 0.9f);
+            dynamicSourceText.color = new Color(0.25f, 0.25f, 0.25f);
+        }
+        else if (button == 2)
+        {
+            quietSourceText.color = new Color(0.9f, 0.9f, 0.9f);
+            calmSourceText.color = new Color(0.9f, 0.9f, 0.9f);
+            dynamicSourceText.color = new Color(0.25f, 0.25f, 0.25f);
+        }
+        else if (button == 3)
+        {
+            quietSourceText.color = new Color(0.9f, 0.9f, 0.9f);
+            calmSourceText.color = new Color(0.9f, 0.9f, 0.9f);
+            dynamicSourceText.color = new Color(0.9f, 0.9f, 0.9f);
+        }
+    }
+
+    /*
+    public void DisableMusic()
+    {
+        quietSourceText.color = new Color(0.5f, 0.5f, 0.5f);
+        calmSourceText.color = new Color(0.5f, 0.5f, 0.5f);
+        dynamicSourceText.color = new Color(0.5f, 0.5f, 0.5f);
+
+        foreach (Text intText in intensityText)
+        {
+            intText.color = new Color(0.5f, 0.5f, 0.5f);
+        }
+
+        foreach (Image intbg in intensityBackground)
+        {
+            if (intbg.fillAmount > 0.2f)
+            {
+                StartCoroutine(FillBackground(intbg, false));
+            }
+        }
+
+        StartCoroutine(Camera.main.GetComponent<SoundManager>().FadeOut(Camera.main.GetComponent<SoundManager>().quietSource));
+        StartCoroutine(Camera.main.GetComponent<SoundManager>().FadeOut(Camera.main.GetComponent<SoundManager>().mediumSource));
+        StartCoroutine(Camera.main.GetComponent<SoundManager>().FadeOut(Camera.main.GetComponent<SoundManager>().dynamicSource));
+    }
+    */
+
+
+    IEnumerator FillBackground(Image bg, bool fill)
+    {
+        if (fill)
+        {
+            while (bg.fillAmount < 1)
+            {
+                bg.fillAmount += fillSpeed * Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            yield break;
+
+        }
+        else
+        {
+            while (bg.fillAmount > 0)
+            {
+                bg.fillAmount -= fillSpeed * Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            yield break;
+        }
+    }
+}
---
+++ DynamicAudio-UnityProject/Assets/SoundManager.cs
@@ -0,0 +1,208 @@
+﻿//SoundManager by Jordi vd Hulst
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    //The state of intensity
+    public enum AudioLevel
+    {
+        Quiet,
+        Calm,
+        Medium,
+        Dynamic
+    };
+
+    public AudioLevel audioLevel;
+
+    //Global source
+    AudioSource _sound;
+
+    //Settings
+    public float bgmVolume;
+    public float fadeSpeed;
+
+    //Source for the intensities
+    public AudioSource quietSource;
+    public AudioSource mediumSource;
+    public AudioSource dynamicSource;
+
+    //What to play
+    public AudioClip[] currentlyPlaying;
+
+
+    //  --- Audio Tracks ---
+    public AudioClip[] island;
+    public AudioClip[] intro;
+    public AudioClip[] conversation;
+    public AudioClip[] house;
+
+    // ----------------------
+
+
+    void Awake()
+    {
+        _sound = GetComponent<AudioSource>();
+    }
+
+    //General settings for this project
+    void Start()
+    {
+        bgmVolume = 0.5f;
+        fadeSpeed = 0.4f;
+        SetLevel(AudioLevel.Calm);
+    }
+
+
+    void Update()
+    {
+        //Hotkeys for testing
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetLevel(AudioLevel.Calm);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetLevel(AudioLevel.Quiet);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetLevel(AudioLevel.Medium);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetLevel(AudioLevel.Dynamic);
+        }
+    }
+
+    //Change the currently playing BGM with a fade transition
+    public void ChangeBGM(AudioClip[] clips, AudioLevel level)
+    {
+        //Fade out the current audio and start fading in the new.
+        if(clips != currentlyPlaying)
+        {
+            StartCoroutine(FadeOutClips(clips));
+        }
+        else
+        {
+            currentlyPlaying = clips;
+            for (int i = 0; i < currentlyPlaying.Length; i++)
+            {
+                if (i == 0)
+                {
+                    quietSource.clip = currentlyPlaying[0];
+                }
+                if (i == 1)
+                {
+                    mediumSource.clip = currentlyPlaying[1];
+                }
+                if (i == 2)
+                {
+                    dynamicSource.clip = currentlyPlaying[2];
+                }
+            }
+
+            SetLevel(level);
+        }
+    }
+
+    //Change the intensity of the music
+    public void SetLevel(AudioLevel level)
+    {
+        if(level == AudioLevel.Quiet)
+        {
+            StartCoroutine("FadeOut", mediumSource);
+            StartCoroutine("FadeOut", dynamicSource);
+            StartCoroutine("FadeIn", quietSource);
+        }
+        if (level == AudioLevel.Calm)
+        {
+            StartCoroutine("FadeIn", mediumSource);
+            StartCoroutine("FadeOut", dynamicSource);
+            StartCoroutine("FadeOut", quietSource);
+        }
+        if (level == AudioLevel.Medium)
+        {
+            StartCoroutine("FadeIn", mediumSource);
+            StartCoroutine("FadeOut", dynamicSource);
+            StartCoroutine("FadeIn", quietSource);
+        }
+        if (level == AudioLevel.Dynamic)
+        {
+            StartCoroutine("FadeIn", mediumSource);
+            StartCoroutine("FadeIn", dynamicSource);
+            StartCoroutine("FadeIn", quietSource);
+        }
+        
+    }
+
+    //Play voice acting
+    public void PlaySpeech(string speech)
+    {
+        _sound.Stop();
+        AudioClip speechToPlay;
+        speechToPlay = Resources.Load<AudioClip>(speech);
+        _sound.PlayOneShot(speechToPlay);
+    }
+
+    //Fade out all music to play new music
+    public IEnumerator FadeOutClips(AudioClip[] newClips)
+    {
+        while (quietSource.volume > 0 && mediumSource.volume > 0 && dynamicSource.volume > 0)
+        {
+            quietSource.volume -= Time.deltaTime * fadeSpeed; ;
+            mediumSource.volume -= Time.deltaTime * fadeSpeed; ;
+            dynamicSource.volume -= Time.deltaTime * fadeSpeed; ;
+            yield return new WaitForEndOfFrame();
+        }
+        
+        currentlyPlaying = newClips;
+
+        for(int i = 0; i < currentlyPlaying.Length; i++)
+        {
+            if(i == 0)
+            {
+                quietSource.clip = currentlyPlaying[0];
+            }
+            if(i == 1)
+            {
+                mediumSource.clip = currentlyPlaying[1];
+            }
+            if(i == 2)
+            {
+                dynamicSource.clip = currentlyPlaying[2];
+            }
+        }
+        quietSource.Play();
+        mediumSource.Play();
+        dynamicSource.Play();
+
+        Debug.Log("set quiet now");
+        SetLevel(AudioLevel.Quiet);
+        yield break;
+    }
+
+    //Fade in new source
+    public IEnumerator FadeIn(AudioSource source)
+    {
+        while (source.volume < bgmVolume)
+        {
+            source.volume += Time.deltaTime * fadeSpeed;
+            yield return new WaitForEndOfFrame();
+        }
+        yield break;
+    }
+
+    //Fade out only one source
+    public IEnumerator FadeOut(AudioSource source)
+    {
+        while(source.volume > 0)
+        {
+            source.volume -= Time.deltaTime * fadeSpeed;
+            yield return new WaitForEndOfFrame();
+        }
+        yield break;
+    }
+}
