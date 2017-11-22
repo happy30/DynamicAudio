668f41b8dea4d6623f17dc2a5f13204bb3c4de59
Jordi -happy30- <jordivdh@hotmail.com>
2017-11-22 02:07:32 +0100

    Added portfolio piece

---
+++ _Portf_SoundManager.cs
@@ -0,0 +1,50 @@
+﻿//Change the intensity of the music
+public void SetLevel(AudioLevel level)
+{
+    if (level == AudioLevel.Quiet)
+    {
+        StartCoroutine("FadeOut", mediumSource);
+        StartCoroutine("FadeOut", dynamicSource);
+        StartCoroutine("FadeIn", quietSource);
+    }
+    if (level == AudioLevel.Calm)
+    {
+        StartCoroutine("FadeIn", mediumSource);
+        StartCoroutine("FadeOut", dynamicSource);
+        StartCoroutine("FadeOut", quietSource);
+    }
+    if (level == AudioLevel.Medium)
+    {
+        StartCoroutine("FadeIn", mediumSource);
+        StartCoroutine("FadeOut", dynamicSource);
+        StartCoroutine("FadeIn", quietSource);
+    }
+    if (level == AudioLevel.Dynamic)
+    {
+        StartCoroutine("FadeIn", mediumSource);
+        StartCoroutine("FadeIn", dynamicSource);
+        StartCoroutine("FadeIn", quietSource);
+    }
+}
+
+//Fade in new intensity level
+public IEnumerator FadeIn(AudioSource source)
+{
+    while (source.volume < bgmVolume)
+    {
+        source.volume += Time.deltaTime * fadeSpeed;
+        yield return new WaitForEndOfFrame();
+    }
+    yield break;
+}
+
+//Fade out one intensity level
+public IEnumerator FadeOut(AudioSource source)
+{
+    while (source.volume > 0)
+    {
+        source.volume -= Time.deltaTime * fadeSpeed;
+        yield return new WaitForEndOfFrame();
+    }
+    yield break;
+}
